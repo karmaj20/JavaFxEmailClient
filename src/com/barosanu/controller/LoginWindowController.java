80f901646220df7b636f55285b5b2607b8012626
Karol <kmajchrzak1999@gmail.com>
2021-04-16 18:35:29 +0200

    Configure JavaMail setup, email account setup and the login service

--- src/com/barosanu/controller/LoginWindowController.java
+++ src/com/barosanu/controller/LoginWindowController.java
@@ -1,6 +1,8 @@
 package com.barosanu.controller;
 
 import com.barosanu.EmailManager;
+import com.barosanu.controller.services.LoginService;
+import com.barosanu.model.EmailAccount;
 import com.barosanu.view.ViewFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -25,11 +27,34 @@ public class LoginWindowController extends BaseController {
 
     @FXML
     void loginButtonAction() {
+        if (fieldsAreValid()) {
+            EmailAccount emailAccount = new EmailAccount(emailAddressField.getText(), passwordField.getText());
+            LoginService loginService = new LoginService(emailAccount, emailManager);
+            EmailLoginResult emailLoginResult = loginService.login();
+
+            switch (emailLoginResult) {
+                case SUCCESS:
+                    System.out.println("login succesful!!!" + emailAccount);
+                    return;
+            }
+        }
         System.out.println("loginButtonAction!!");
         viewFactory.showMainWindow();
         Stage stage = (Stage) errorLabel.getScene().getWindow();
         viewFactory.closeStage(stage);
+    }
 
+    private boolean fieldsAreValid() {
+        if (emailAddressField.getText().isEmpty()) {
+            errorLabel.setText("Please fill email");
+            return false;
+        }
+        if (passwordField.getText().isEmpty()) {
+            errorLabel.setText("Please fill password");
+            return false;
+        }
+
+        return true;
     }
 
 }
