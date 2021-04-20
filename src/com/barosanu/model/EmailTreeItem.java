28cfaccae16214ed2fc670c00669232e96323110
Karol <kmajchrzak1999@gmail.com>
2021-04-20 16:21:33 +0200

    email actions, created message listener, updating folders with new messages, receiving mails, interacting with emails, deleting emails

--- src/com/barosanu/model/EmailTreeItem.java
+++ src/com/barosanu/model/EmailTreeItem.java
@@ -27,6 +27,11 @@ public class EmailTreeItem<String> extends TreeItem<String> {
     }
 
     public void addEmail(Message message) throws MessagingException {
+        EmailMessage emailMessage = fetchMessage(message);
+        emailMessages.add(emailMessage);
+    }
+
+    private EmailMessage fetchMessage(Message message) throws MessagingException {
         boolean messageIsRead = message.getFlags().contains(Flags.Flag.SEEN);
         EmailMessage emailMessage = new EmailMessage(
                 message.getSubject(),
@@ -37,11 +42,11 @@ public class EmailTreeItem<String> extends TreeItem<String> {
                 messageIsRead,
                 message
         );
-        emailMessages.add(emailMessage);
         if (!messageIsRead) {
             incrementMessagesCount();
         }
-        System.out.println("added to " + name + " " + message.getSubject());
+
+        return emailMessage;
     }
 
     public void incrementMessagesCount() {
@@ -49,9 +54,19 @@ public class EmailTreeItem<String> extends TreeItem<String> {
         updateName();
     }
 
+    public void decrementMessagesCount() {
+        unreadMessagesCount--;
+        updateName();
+    }
+
     private void updateName() {
         if (unreadMessagesCount > 0) {
             this.setValue((String)(name + "(" + unreadMessagesCount + ")"));
         }
+    }
+
+    public void addEmailToTop(Message message) throws MessagingException {
+        EmailMessage emailMessage = fetchMessage(message);
+        emailMessages.add(0, emailMessage);
     }
 }
